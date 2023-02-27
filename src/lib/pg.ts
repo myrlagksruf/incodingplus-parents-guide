edde7d675966b186461d4f9676d9eb4527fdf991
myrlagksruf <myrlagksruf@gmail.com>
2023-02-27 21:38:24 +0900

    숙제명까지 받아오도록 설정

--- src/lib/pg.ts
+++ src/lib/pg.ts
@@ -9,15 +9,18 @@ const pool = new pg.Pool({
     port:Number(import.meta.env.VITE_DB_PORT ?? process.env.VITE_DB_PORT),
 });
 
+interface iHome{
+    아이디: string;
+    학생이름: string;
+    수업명: string;
+    숙제명:string;
+    과목: string;
+};
+
 
 export const getInfo = async (id:string):Promise<{
     status:true;
-    res:pg.QueryResult<{
-        아이디: string;
-        학생이름: string;
-        수업명: string;
-        과목: string;
-    }>
+    res:pg.QueryResult<iHome>
 }|{
     status:false;
     reason:string;
@@ -25,22 +28,18 @@ export const getInfo = async (id:string):Promise<{
     let client:PoolClient|null = null;
     try{
         client = await pool.connect();
-        /** @type {import('pg').QueryResult<{id:string}>} */
-        const res = await client.query<{
-            아이디:string;
-            학생이름:string;
-            수업명:string;
-            과목:string;
-        }>(`
+        const res = await client.query<iHome>(`
         select
             ss2.id as 아이디,
             ss2.name as 학생이름,
             sc2.name as 수업명,
+            scm2.name as 숙제명,
             sc2.category as 과목
         from (
             select
                 ss.user_id as ud,
                 sc.root_id as rd,
+               	scm.root_id as srd,
                 max(sc.last_modified) as md
             from svc_submit ss
             join svc_course_material scm
@@ -48,12 +47,14 @@ export const getInfo = async (id:string):Promise<{
             join svc_course sc
                 on scm.root_id = any(sc.material_ids)
             where ss.registered_at > '2023-02-01 00:00:00'
-            group by ss.user_id, sc.root_id
+            group by ss.user_id, sc.root_id, scm.root_id 
         ) as tt
         join svc_course sc2 on
             tt.rd = sc2.root_id and tt.md = sc2.last_modified
         join svc_student ss2 on
             ss2.id = tt.ud and ss2.status = '재원'
+        join svc_course_material scm2 on
+        	scm2.root_id = tt.srd
         where
             sc2.name like '[%' and ss2.id='${id}'
         order by sc2.registered_at asc
--- src/routes/[id]/+page.svelte
+++ src/routes/[id]/+page.svelte
@@ -2,7 +2,27 @@
     import type { PageData } from "./$types";
 
     export let data:PageData
-    console.log(data.info);
+    const map:{[key:string]:{[key:string]:string[]}} = {
+    };
+
+    const error:typeof data['info'] = [];
+
+    for(let i of data.info){
+        if(!map[i.과목]){
+            map[i.과목] = {};
+        }
+        let subject = i.숙제명.match(/\([가-힣]+\)\s*\[([\d\.가-힣]+)\]\s*/);
+        if(!subject){
+            error.push(i);
+            continue;
+        }
+        let raw = i.숙제명.replace(subject[0], '');
+        if(!map[i.과목][subject[1]]){
+            map[i.과목][subject[1]] = [];
+        }
+        const arr = map[i.과목][subject[1]];
+        if(arr.includes())
+    }
 </script>
 
 {#each data.info as info}
